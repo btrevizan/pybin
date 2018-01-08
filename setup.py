edd0c33f8629e5f51cb5edd0d8e4e0dc0c523515
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-01-08 11:17:42 -0200

    Package files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='pybin',
+    packages=['pybin'],
+    version='0.1',
+    description='A binary file\'s manager in Python 3.',
+    author='Bernardo Trevizan',
+    author_email='trevizanbernardo@gmail.com',
+    url='https://github.com/btrevizan/pybin',
+    keywords='struct,file,binary,python'
+)
