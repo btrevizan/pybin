3015d78dad3aa8e23ee55920f3af79ff64edad70
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-01-08 11:27:26 -0200

    Rename package

--- pybin/__init__.py
+++
@@ -1,1 +0,0 @@
-from .pybin import StructFile
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(
-    name='pybin',
-    packages=['pybin'],
+    name='pystrct',
+    packages=['pystrct'],
     version='0.1',
     description='A binary file\'s manager in Python 3.',
     author='Bernardo Trevizan',
     author_email='trevizanbernardo@gmail.com',
-    url='https://github.com/btrevizan/pybin',
+    url='https://github.com/btrevizan/pystrct',
     keywords='struct,file,binary,python'
 )
